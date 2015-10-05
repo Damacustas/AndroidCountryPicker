39a0ac700885aa07f731adfa2c978fe4ae89e1d9
Lars Bokkers <LBokkers@kpnmail.nl>
2015-10-06 00:40:46 +0200

    Removed dependency on support libraries. Removed example application.

--- CountryPicker/src/main/java/com/countrypicker/CountryPicker.java
+++ CountryPicker/src/main/java/com/countrypicker/CountryPicker.java
@@ -1,7 +1,7 @@
 package com.countrypicker;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
